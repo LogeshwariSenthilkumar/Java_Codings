1217f013098fadae5872b766e8cccb6b6d31d336
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-05-15 14:52:20 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,27 @@
+class Sample
+{
+	public static void main(String[] args)
+	{   //Numerical:
+		byte b =-128;
+		System.out.println(b);
+		short s=32767;
+		System.out.println(s);
+		int i=10;
+		System.out.println(i);
+		long l=6380931960l;
+		System.out.println(l);
+		
+		//Decimal:
+		float f=3.56555f;
+		System.out.println(f);
+		double d=3.56565647;
+		System.out.println(d);
+		
+		char c='L';
+		System.out.println(c);
+		
+		String ss="Logeshwari";
+		System.out.println(ss);
+		
+	}
+}
