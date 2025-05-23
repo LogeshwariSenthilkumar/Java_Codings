0d7f7ab1e75f8d9c978de98b437effd368aecf2e
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-05-23 22:40:09 +0530

    Add files via upload

---
+++ Scanner.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+class Example {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//Numerical:
+		/*System.out.println("Enter the byte value : ");
+		byte a = sc.nextByte();
+		System.out.println("Byte Value is : " + a);
+		System.out.println("Enter the short value : ");
+		short b = sc.nextShort();
+		System.out.println("Short Value is : " + b);
+		System.out.println("Enter the int value : ");
+		int c = sc.nextInt();
+		System.out.println("Int Value is  : " + c);
+		System.out.println("Enter the phone no : ");
+		long d = sc.nextLong();
+		System.out.println("Long Value is : " + d);
+		*/
+		
+		//Decimal:
+		/*System.out.println("Enter the float value : ");
+		float f1 = sc.nextFloat();
+		System.out.println("Float Value is  : " + f1);
+		System.out.println("Enter the double value : ");
+		double d1 = sc.nextDouble();
+		System.out.println("Double Value is : " + d1);
+		*/
+		
+		//Boolean:
+		/*System.out.println("Enter the boolean value : ");
+		boolean val = sc.nextBoolean();
+		System.out.println("Boolean Value : " + val);
+		*/
+		
+		//String:
+		/*System.out.println("Enter the name : ");
+		String name2 = sc.nextLine();
+		System.out.println("String value2 : " + name2);*/
+		
+		//charAt:
+		System.out.println("Enter String:");
+		char name1=sc.next().charAt(4);
+		System.out.println("The Char is:"+name1);
+		
+	}
+
+}
