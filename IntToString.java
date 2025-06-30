4f33851a64dc203915914c246f26f41aefac7fcd
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-06-30 11:36:18 +0530

    Add files via upload

---
+++ IntToString.java
@@ -0,0 +1,9 @@
+class Demo{
+    public static void main(String[] args){
+        int number=10;
+        String s=Integer.toString(number);
+        System.out.println(s);
+        String p=s+" Hi";
+        System.out.println(p);
+    }
+}
