f51f95753867095302d91b9d3831f996728b581d
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-02 14:45:05 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,8 @@
+class Stringcls{
+    public static void main(String[] args){
+        StringBuffer sb=new StringBuffer("Hello");
+        System.out.println(sb);
+        StringBuilder sb1=new StringBuilder("Logu");
+        System.out.println(sb1);
+    }
+}
