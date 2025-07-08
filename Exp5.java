0d8bf3cafa715566742a2a6249a0274f33e6a825
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-08 12:29:44 +0530

    Add files via upload

---
+++ Exp5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Expcls5{
+    public static void main(String[] args){
+        //runtime exceptions:
+        System.out.println("Welcome");
+
+        // int c=10/0;      ====> 1.ArithmeticException
+
+        // int[] a={1,2,3,4,5};
+        // System.out.println(a[10]); =====> 2.ArrayIndexOutOfBoundsException
+
+        // String s1=null;
+        // System.out.println(s1.toLowerCase()); =====> 3.NullPointerException
+
+        // String s2="Hello";
+        // System.out.println(s2.charAt(10));  =====> 4.StringIndexOutOfBoundsException
+
+        // Scanner sc=new Scanner(System.in);
+        // int b=sc.nextInt();                 =======> 5.InputMismatchException
+
+        System.out.println("End of program");
+    }
+}
