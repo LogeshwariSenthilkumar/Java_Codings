8d7b9935819e8cce20815bc82174b2376816e4e0
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-10 14:43:22 +0530

    Add files via upload

---
+++ Codes3.java
@@ -0,0 +1,12 @@
+class Demo{
+    public static void main(String[] args){
+          int num1=10;
+          int num2=10;
+          if(num1==num2){
+            System.out.println("Equal");
+          }
+          else{
+            System.out.println("Not equal");
+          }
+    }
+}
