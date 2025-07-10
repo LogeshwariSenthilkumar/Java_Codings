47dcdb3be1df7ae81a4fca1486758910399eb304
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-10 14:43:00 +0530

    Add files via upload

---
+++ Codes2.java
@@ -0,0 +1,12 @@
+class Demo{
+    public static void main(String[] args){
+          String s1="Logu";
+          String s2="Logu";
+          if(s1.equals(s2)){
+            System.out.println("Equal");
+          }
+          else{
+            System.out.println("Not equal");
+          }
+    }
+}
