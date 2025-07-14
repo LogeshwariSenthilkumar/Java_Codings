8cb7dfee90ee1b67939eeb676466adde34d6b285
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-14 12:11:25 +0530

    Add files via upload

---
+++ Codes19.java
@@ -0,0 +1,12 @@
+class Addition{
+    Addition(){
+        System.out.println("Empty");
+    }
+    int Addition(){
+    return 20;
+    }
+    public static void main(String[] args){
+     Addition a=new Addition();
+     System.out.println( a.Addition());
+    }
+}
