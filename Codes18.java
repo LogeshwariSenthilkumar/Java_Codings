1f84a2b1418e86d79c7a155ed7dc66c27f88e7ef
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-15 10:46:18 +0530

    Add files via upload

---
+++ Codes18.java
@@ -0,0 +1,7 @@
+import java.io.File;
+class Demo{
+    public static void main(String[] args){
+         File f1=new File("C:\\Users\\LOGESHWARISENTHIL\\Documents\\Java\\Folder1");
+         f1.mkdir();
+    }
+}
