bd8925a4aaaa1ef1a2652d028cc7d3c862979883
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-18 15:41:29 +0530

    Add files via upload

---
+++ DateTime.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+class Main{
+    public static void main(String[] args){
+        LocalDateTime datetime=LocalDateTime.now();
+        System.out.println("DateTime : "+datetime);
+    }
+}
