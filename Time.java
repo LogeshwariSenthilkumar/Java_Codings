48f1b14440d8f4fc92cb1dfa1a9564ec1404ab04
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-18 15:41:14 +0530

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,7 @@
+import java.time.LocalTime;
+class Main{
+    public static void main(String[] args){
+        LocalTime time=LocalTime.now();
+        System.out.println("Time Now : "+time);
+    }
+}
