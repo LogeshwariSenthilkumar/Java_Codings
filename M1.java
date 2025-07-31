4d060ad94fee0714aef95f48498cbc139376a8ba
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-07-31 10:37:03 +0530

    Add files via upload

--- M1.java
+++ M1.java
@@ -1,31 +1,60 @@
-import java.util.Scanner;
-class MultiArray{
+class ATM{
+    int balance=0;
+    synchronized void withDraw(String name,int amount){
+    while(balance<amount){
+        try{
+            System.out.println("waiting ....");
+          wait();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    balance-=amount;
+    System.out.println("Amount "+amount+" Successfully Withdrawn by "+name);
+    System.out.println("Remaining Balance : "+balance);
+    }
+    synchronized void deposit(int amount){
+        balance+=amount;
+        System.out.println("Amount "+amount+" Deposited Successfully");
+        System.out.println("Available Balance : "+balance);
+        notifyAll();
+    }
+}
+class Customer extends Thread{
+    ATM ob;
+    String name;
+    int amount;
+    Customer(ATM ob,String name,int amount){
+        this.ob=ob;
+        this.name=name;
+        this.amount=amount;
+    }
+    public void run(){
+        ob.withDraw(name,amount);
+    }
+}
+class Bank extends Thread{
+    ATM ob;
+    int amount;
+    Bank(ATM ob,int amount){
+        this.ob=ob;
+        this.amount=amount;
+    }
+    public void run(){
+        try{
+            Thread.sleep(3000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        ob.deposit(amount);
+    }
+}
+class BankATM{
     public static void main(String[] args){
-        // int[][] arr={{1,2,3},{1,2,3}};   =========> Litral way
-        // for(int i=0;i<arr.length;i++){
-        //     for(int j=0;j<arr[i].length;j++){
-        //         System.out.print(arr[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        
-        Scanner sc=new Scanner(System.in);                 //===========> Non-Litral Way
-        System.out.println("Enter the Array Elements:");
-        int[][] arr=new int[3][3];
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr.length;j++){
-                arr[i][j]=sc.nextInt();
-            }
-            System.out.println();
-        }
-        System.out.println("\nThe Array Elements are:");
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-
+     ATM atm=new ATM();
+     new Customer(atm,"Logu",2000).start();
+     new Customer(atm,"Janu",3000).start();
+     new Customer(atm,"Bara",4000).start();
+     new Bank(atm,15000).start();
     }
 }
