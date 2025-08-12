aea93975baf0602c74469f6c3a1505489e3067d8
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-12 16:56:26 +0530

    Add files via upload

---
+++ Codes68.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class ArrayList4{
+    public static void main(String[] args){
+        ArrayList<Integer> al=new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        // System.out.println(al);
+        al.add(2,80);
+        // System.out.println(al);
+        ArrayList<Integer> al2=new ArrayList<>();
+        al2.addAll(al);
+        al2.add(70);
+        al2.add(100);
+        al2.addAll(3,al);
+        System.out.println(al2);
+        al2.set(0,5);
+        System.out.println(al2);
+        // al2.replaceAll(f->f.sort());
+        // System.out.println(al2);
+        Collections.sort(al2);
+        System.out.println(al2);
+ 
+
+    }
+}
