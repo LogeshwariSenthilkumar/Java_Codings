e9eab5446c5d9c09a84d96d327d1463f1dabb9b8
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-12 16:57:57 +0530

    Add files via upload

---
+++ Codes71.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class ArrayList7{
+    public static void main(String[] args){
+        ArrayList<Integer> al=new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        // System.out.println(al);
+        al.add(2,80);
+        // System.out.println(al);
+        ArrayList<Integer> al2=new ArrayList<>();
+        al2.addAll(al);
+        al2.add(70);
+        al2.add(100);
+        al2.addAll(3,al);
+        System.out.println(al2);
+        // List li=al2.subList(0,4);
+        // System.out.println(li);
+        // ListIterator i=al2.listIterator();
+        // while(i.hasPrevious()){
+        //     System.out.println(i.previous());
+        // }
+        Spliterator s=al2.spliterator();
+        s.forEachRemaining(System.out::println);
+    }
+}
