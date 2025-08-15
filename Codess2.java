7bbfb7a61f6c6106a8ff99258b1c944de0f15de3
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-15 22:47:44 +0530

    Add files via upload

---
+++ Codess2.java
@@ -0,0 +1,10 @@
+class Array1{
+    public static void main(String[] args){
+        int[] nums={1,2,3,4,5};
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        System.out.println("Sum of The Array Elements : "+sum);
+    }
+}
