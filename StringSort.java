a3f9ebf33b48122763e226d743455f729ec3199e
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-22 20:34:15 +0530

    Add files via upload

---
+++ StringSort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Array1{
+    public static void main(String[] args){
+        String[] strarr={"Logu","Chitra","Janani","Dhivya"};
+        Arrays.sort(strarr);
+        for(String s:strarr){
+            System.out.print(s+" ");
+        }
+    }
+}
