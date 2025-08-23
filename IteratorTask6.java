ed6e2f7375c1d040a3bed027ecc39bd41c33c191
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-23 16:56:28 +0530

    Add files via upload

---
+++ IteratorTask6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Task6{
+    public static void main(String[] args){
+        ArrayList<String> al=new ArrayList<>();
+        al.add("Logu");
+        al.add("Janu");
+        al.add("Dhivya");
+        al.add("Barani");
+        al.add("Aishu");
+        System.out.println(al);
+        for(String s:al){
+            System.out.println(s.toUpperCase());
+        }
+
+    }
+}
