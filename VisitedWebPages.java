aa7e57a3ca12a52a3a682e3a826b390817666333
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-26 22:24:06 +0530

    Add files via upload

---
+++ VisitedWebPages.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class VisitedWebPages{
+    public static void main(String[] args){
+        LinkedHashSet<String> lhs=new LinkedHashSet<>();
+       lhs.add("Home Page");
+       lhs.add("Page 1");
+       lhs.add("Page 2");
+       lhs.add("Page 3");
+       lhs.add("Page 4");
+       System.out.println(lhs);
+    }
+}
