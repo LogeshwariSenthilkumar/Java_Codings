986ab5d4345c943afc3f954ca1eaf270c852fbdf
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-26 22:21:02 +0530

    Add files via upload

---
+++ SortedStudentNames.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class SortedStudentNames{
+    public static void main(String[] args){
+        TreeSet<String> ts=new TreeSet<>();
+        ts.add("Logeshwari");
+        ts.add("Dhivya");
+        ts.add("Janani");
+        ts.add("Barani");
+        ts.add("Chitra");
+        System.out.println(ts);
+    }
+}
