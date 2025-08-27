e0005490bc6e6f20ba5dfba8d8ba01101039bb16
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-27 21:39:55 +0530

    Add files via upload

---
+++ ClockWiseShift.java
@@ -0,0 +1,14 @@
+class Array45{
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        int last=arr[arr.length-1];
+        for(int i=arr.length-1;i>=1;i--){
+            arr[i]=arr[i-1];
+        }
+        arr[0]=last;
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+
+    }
+}
