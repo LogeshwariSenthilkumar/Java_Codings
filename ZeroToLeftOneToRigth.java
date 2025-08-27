b10867fe7a0ed6c02ef2d1dd12cd01b1a0121021
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-27 21:39:03 +0530

    Add files via upload

---
+++ ZeroToLeftOneToRigth.java
@@ -0,0 +1,17 @@
+class Array42{
+    public static void main(String[] args){
+        int[] arr={1,0,0,1,0,1,0,1};
+       for(int i=0;i<arr.length;i++){
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+       }
+       for(int i:arr){
+        System.out.print(i+" ");
+       }
+    }
+}
