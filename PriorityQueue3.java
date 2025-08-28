6a0d1bac5310c15ae13b3e45b9deabdb418bca89
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-28 22:28:33 +0530

    Add files via upload

---
+++ PriorityQueue3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Priority3{
+    public static void main(String[] args){
+        PriorityQueue<String> p=new PriorityQueue<>();
+        p.offer("Logu");
+        p.offer("Dhivya");
+        p.offer("Chitra");
+        p.offer("Barani");
+        p.offer("Janani");
+        p.offer("Aishu");
+        System.out.println(p);
+    }
+}
