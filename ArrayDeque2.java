93666bbb50c550c41f8f9d0b1b269bd99326e251
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-08-28 22:29:08 +0530

    Add files via upload

---
+++ ArrayDeque2.java
@@ -0,0 +1,15 @@
+//as a Queue:
+import java.util.*;
+class Demo2{
+    public static void main(String[] args){
+        ArrayDeque<Integer> ad=new ArrayDeque<>();
+        ad.offer(30);
+        ad.offer(15);
+        ad.offer(40);
+        ad.offer(20);
+        ad.offer(10);
+        ad.offer(65);
+        ad.offer(10);
+        System.out.println(ad);
+    }
+}
