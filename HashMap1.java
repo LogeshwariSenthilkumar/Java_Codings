c669717c7d7ced310a39f118ae5d31b33f2d0fc6
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-01 16:33:22 +0530

    Add files via upload

---
+++ HashMap1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Demo{
+    public static void main(String[] args){
+        HashMap<Integer,String> hm=new HashMap<>();
+        hm.put(101,"Logu");
+        hm.put(102,"Janu");
+        hm.put(103,"Dhivya");
+        hm.put(104,"Chitra");
+        hm.put(105,"Barani");
+        System.out.println(hm);
+    }
+}
