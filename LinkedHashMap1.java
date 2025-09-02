0183c2666039c6c76bcf730e2fc8a61bd9192e5b
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-02 15:20:18 +0530

    Add files via upload

---
+++ LinkedHashMap1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Demo4{
+    public static void main(String[] args){
+        LinkedHashMap<Integer,String> lk=new LinkedHashMap<>();
+        lk.put(102,"Janu");
+        lk.put(101,"Logu");
+        lk.put(103,"Dhivya");
+        System.out.println(lk);
+    }
+}
