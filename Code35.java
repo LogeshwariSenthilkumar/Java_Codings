75d6b403ebac8ab904de686b43c3aaa7c6532aa1
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-02 15:18:19 +0530

    Add files via upload

---
+++ Code35.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo4{
+    public static void main(String[] args){
+        TreeMap<Integer,String> tm=new TreeMap<>();
+        tm.put(30,"Apple");
+        tm.put(20,"Orange");
+        tm.put(50,"Cherry");
+        tm.put(10,"Mango");
+        tm.put(40,"Banana");
+       
+        System.out.println(tm);
+    }
+}
