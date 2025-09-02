cd8165e26d551690d1f4ea9b3e2eb7b5a1cee00d
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-02 15:16:55 +0530

    Add files via upload

---
+++ Code32.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Demo{
+    public static void main(String[] args){
+        HashMap<Integer,String> hm=new HashMap<>();
+        hm.put(10,"Logu");
+        hm.put(20,"Janu");
+        hm.put(30,"Hari");
+        hm.put(40,"Bharath");
+        hm.put(50,"Chitra");
+        System.out.println(hm);
+    }
+}
