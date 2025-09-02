50610e9edab48f3448c6e248725481040e9cd678
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-02 15:20:36 +0530

    Add files via upload

---
+++ TreeMap1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Demo5{
+    public static void main(String[] args){
+        TreeMap<Integer,String> tm=new TreeMap<>();
+        tm.put(102,"Logu");
+        tm.put(101,"Janu");
+        tm.put(100,"Chitra");
+        tm.put(103,"Dhivya");
+        System.out.println(tm);
+    }
+}
