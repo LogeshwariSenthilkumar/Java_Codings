4fae142cc94b0d8fc146e386f51fb56b8a950bf3
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-04 17:24:33 +0530

    Add files via upload

---
+++ DictionaryApptm.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Dict{
+    public static void main(String[] args){
+        TreeMap<String,String> word=new TreeMap<>();
+        word.put("Serene","calm/peaceful");
+        word.put("Evoke","bring a memory or feeling into mind");
+        word.put("Diligent","showing hard work and care");
+        word.put("Transient","lasting for a short time");
+        word.put("Ample","more than enough");
+        System.out.println(word);
+    }
+}
