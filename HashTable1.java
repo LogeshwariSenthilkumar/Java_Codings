6e1ce72a2deedbaf35903f0532d1aad611ae1a45
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-05 17:05:30 +0530

    Add files via upload

---
+++ HashTable1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo{
+    public static void main(String[] args){
+        Hashtable<Integer,String> ht=new Hashtable<>();
+        ht.put(10,"Logu");
+        ht.put(20,"Hari");
+        ht.put(30,"Janu");
+        ht.put(40,"Barani");
+        ht.put(50,"Chitra");
+        ht.put(60,"Senthil");
+        System.out.println(ht);
+    }
+}
