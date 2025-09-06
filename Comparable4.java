b52d05ead30d81e0cc324930bcfce1eb3e2bf643
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-06 15:29:37 +0530

    Add files via upload

---
+++ Comparable4.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Student implements Comparable<Student>{
+    int id;
+    String name;
+    Student(int id,String name){
+        this.id=id;
+        this.name=name;
+    }
+    public String toString(){
+        return id+" "+name;
+    }
+    // public int compareTo(Student s){
+    //     if(id<s.id)
+    //     {
+    //         return 1;
+    //     }
+    //     else if(id>s.id){
+    //         return -1;
+    //     }
+    //     else{
+    //         return 0;
+    //     }
+    // }
+    public int compareTo(Student s){
+        return name.compareTo(s.name);
+    }
+   
+}
+class Demo{
+    public static void main(String[] args){
+        LinkedHashSet<Student> al=new LinkedHashSet<>();
+        al.add(new Student(104,"Logu"));
+        al.add(new Student(102,"Dhivya"));
+        al.add(new Student(101,"Janu"));
+        al.add(new Student(105,"Barani"));
+        al.add(new Student(103,"Chitra"));
+        TreeSet<Student> ts=new TreeSet<>(al);
+        // Collections.sort(al);
+        System.out.println(ts);
+    }
+}
