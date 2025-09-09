99ccbcd005576966ce4bf9a1f3c518abf97c12bf
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-09 11:03:35 +0530

    Add files via upload

---
+++ CollectionsMtd2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Demo{
+    public static void main(String[] args){
+        ArrayDeque<Integer> al=new ArrayDeque<>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        System.out.println(al);
+        Collections.sort(al);
+        System.out.println(al);
+        Collections.shuffle(al);
+        System.out.println(al);
+        Collections.swap(al,0,4);
+        System.out.println(al);
+        System.out.println(Collections.min(al));
+        System.out.println(Collections.max(al));
+        Collections.reverse(al);
+        System.out.println(al);
+        Collections.fill(al,10);
+        System.out.println(al);
+        System.out.println(Collections.frequency(al,10));
+        System.out.println(al);
+        Collections.replaceAll(al,10,100);
+        System.out.println(al);
+        // List<Integer> al2=Arrays.asSet(1,2,3,4,5,6);
+        // Collections.copy(al2,al);
+        // System.out.println(al2);
+        
+
+    }
+}
