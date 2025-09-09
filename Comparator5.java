f061b7cfd0b3455db6ce028ae3993182bdd587db
LogeshwariSenthilkumar <146370171+LogeshwariSenthilkumar@users.noreply.github.com>
2025-09-09 11:02:31 +0530

    Add files via upload

---
+++ Comparator5.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Student{
+    int id;
+    String name;
+    Student(int id,String name){
+        this.id=id;
+        this.name=name;
+    }
+    public String toString(){
+        return id+" "+name;
+    }
+}
+class IdSort implements Comparator<Student>{
+    public int compare(Student s1,Student s2){
+        return s1.id-s2.id;
+    }
+}
+class NameSort implements Comparator<Student>{
+    public int compare(Student s1,Student s2){
+        return s1.name.compareTo(s2.name);
+    }
+}
+class Demo{
+    public static void main(String[] args){
+        ArrayDeque<Student> al=new ArrayDeque<>();
+        al.add(new Student(104,"Logu"));
+        al.add(new Student(102,"Dhivya"));
+        al.add(new Student(101,"Janu"));
+        al.add(new Student(105,"Barani"));
+        al.add(new Student(103,"Chitra"));
+        System.out.println("BY ID:");
+        PriorityQueue<Student> pq=new PriorityQueue<>(new IdSort());
+        pq.addAll(al);
+         while(!pq.isEmpty()){
+            System.out.println(pq.poll());
+        }
+         System.out.println("BY NAME:");
+        PriorityQueue<Student> pq2=new PriorityQueue<>(new NameSort());
+        pq2.addAll(al);
+       
+          while(!pq2.isEmpty()){
+            System.out.println(pq2.poll());
+        }
+        
+
+
+    }
+}
